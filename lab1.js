c29ba6fe4b9e8f8eb01de6ff5157f48455fd3fdd
samoilenkomariia <mariia.samoilenko.06@gmail.com>
2024-12-22 21:40:11 +0200

    add delay

--- lab1.js
+++ lab1.js
@@ -4,15 +4,23 @@ const nums = [5, 2, 7, 3, 4];
 
 // console.log(nums.map((x) => x * x));
 
-const asyncMap = (array, callback, callback2) => {
+const asyncMap = (array, callback, finishingCallback, delay) => {
   const result = [];
   let completed = 0;
 
   for (const item of array) {
+    const startTime = Date.now();
     callback(item, (value) => {
-      result.push(value);
-      completed++;
-      if (completed === array.length) callback2(result);
+      const elapsedTime = Date.now() - startTime;
+      const remainingTime = delay - elapsedTime;
+      const handleResult = () => {
+        result.push(value);
+        completed++;
+        if (completed === array.length) finishingCallback(result);
+      };
+      if (remainingTime > 0) {
+        setTimeout(handleResult, 100);
+      }
     });
   }
 };
@@ -23,4 +31,4 @@ const squareNums = (num, done) => {
   }, 50);
 };
 
-asyncMap(nums, squareNums, (result) => console.log(result));
+asyncMap(nums, squareNums, (result) => console.log(result), 100);
