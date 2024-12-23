e53dc5d74f291bdefc566afc2739bdf126816aa6
samoilenkomariia <mariia.samoilenko.06@gmail.com>
2024-12-23 15:53:08 +0200

    add abort controller use case for async await

--- lab3.js
+++ lab3.js
@@ -48,10 +48,17 @@ setTimeout(() => controller.abort(), 500);
 //promise based use case
 asyncMap(nums, 100, signal)
   .then((result) => console.log(result))
-  .catch((error) => console.error(error.message));
+  .catch((error) => console.error("promise err:", error.message));
 
-// //async await based use case
-// (async () => {
-//   const result = await asyncMap(nums, 100);
-//   console.log(result);
-// })();
+//async await based use case
+(async () => {
+  try {
+    const controller2 = new AbortController();
+    const { signal: signal2 } = controller2;
+    setTimeout(() => controller2.abort(), 600);
+    const result = await asyncMap(nums, 100, signal2);
+    console.log(result);
+  } catch (error) {
+    console.error("async-await err:", error.message);
+  }
+})();
