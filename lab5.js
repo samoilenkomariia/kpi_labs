74be5a8ab5df36265282bb1ba2aa3a307b2e9a03
samoilenkomariia <mariia.samoilenko.06@gmail.com>
2024-12-23 21:26:11 +0200

    refactor code a bit and add second listener

--- lab5.js
+++ lab5.js
@@ -1,16 +1,27 @@
 const EventEmitter = require("events");
 
 class MessageEmitter extends EventEmitter {
+  constructor() {
+    super();
+    this.messageCount = 0;
+  }
   sendMessage(message) {
-    this.emit("message", message);
+    this.messageCount++;
+    this.emit("message", { ...message, id: this.messageCount });
   }
 }
 
 const emitter = new MessageEmitter();
 
-setInterval(() => {
-  const message = { text: "some text" };
+const intervalId = setInterval(() => {
+  const message = { text: "some text", timestamp: Date.now() };
   emitter.sendMessage(message);
 }, 1000);
 
-emitter.on("message", (message) => console.log("Received:", message));
+emitter.on("message", (message) => {
+  console.log("First listener received:", message);
+});
+
+emitter.on("message", (message) => {
+  console.log("Second listener received:", message);
+});
