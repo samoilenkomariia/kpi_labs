5cc37967c8ca1aac4646497ab462541c02651426
samoilenkomariia <mariia.samoilenko.06@gmail.com>
2024-12-23 19:55:21 +0200

    changing code a bit

--- lab4.js
+++ lab4.js
@@ -1,23 +1,31 @@
 "use strict";
 
-const fetchData = async function* () {
-  const data = [
-    { id: 1, value: "item1" },
-    { id: 2, value: "item2" },
-    { id: 3, value: "item3" },
-  ];
+const data = [
+  { id: 1, value: "item1" },
+  { id: 2, value: "item2" },
+  { id: 3, value: "item3" },
+];
 
+const data2 = [
+  [1, 2],
+  [2, 3],
+  [4, 5],
+  [5, 6],
+];
+
+const fetchData = async function* (data) {
   for (const item of data) {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     yield item;
   }
 };
 
-const processData = async () => {
-  const dataGenerator = fetchData();
+const processData = async (data) => {
+  const dataGenerator = fetchData(data);
   for await (const item of dataGenerator) {
     console.log("Processing:", item);
   }
 };
 
-processData();
+processData(data);
+processData(data2);
