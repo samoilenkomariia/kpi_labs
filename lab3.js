9db4f57f17ac82b775809d7e5ee8044d857426c5
samoilenkomariia <mariia.samoilenko.06@gmail.com>
2024-12-23 15:28:52 +0200

    integrate abort controller for promise

--- lab3.js
+++ lab3.js
@@ -2,8 +2,15 @@
 
 const nums = [5, 2, 7, 3, 4];
 
-const asyncMap = (array, ms) => {
+const controller = new AbortController();
+const { signal } = controller;
+
+const asyncMap = (array, ms, signal) => {
   return new Promise((resolve, reject) => {
+    if (signal?.aborted) {
+      reject(new Error("asyncMap aborted"));
+      return;
+    }
     setTimeout(() => {
       const promises = array.map((item) => squareNums(item));
 
@@ -12,17 +19,29 @@ const asyncMap = (array, ms) => {
   });
 };
 
-const squareNums = (num) => {
+const squareNums = (num, signal) => {
   return new Promise((resolve) => {
-    setTimeout(() => resolve(num * num), 1000);
+    const timeoutID = setTimeout(() => {
+      if (signal?.aborted) reject(new Error("squareNum aborted"));
+      else resolve(num * num);
+    }, 1000);
+
+    signal?.addEventListener("abort", () => {
+      clearTimeout(timeoutID);
+      reject(new Error("squareNum aborted"));
+    });
   });
 };
 
-//promise based use case
-asyncMap(nums, 100).then((result) => console.log(result));
+setTimeout(() => controller.abort(), 500);
 
-//async await based use case
-(async () => {
-  const result = await asyncMap(nums, 100);
-  console.log(result);
-})();
+//promise based use case
+asyncMap(nums, 100)
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error.message));
+
+// //async await based use case
+// (async () => {
+//   const result = await asyncMap(nums, 100);
+//   console.log(result);
+// })();
