0ec9fca9b61b68ccd23853e89840af918efdae90
samoilenkomariia <mariia.samoilenko.06@gmail.com>
2024-12-29 15:35:48 +0200

    fix

--- lab1.js
+++ lab1.js
@@ -1,29 +1,53 @@
 "use strict";
 
-const nums = [5, 4, 7, 3, 9];
+const nums = [5, 4, "sd", 3, 9];
 
 function asyncMap(array, transformer, callback) {
   let results = [];
   let completed = 0;
+  let isErr = false;
 
   for (let i = 0; i < array.length; i++) {
-    transformer(array[i], i, array, (result) => {
-      results[i] = result;
-      completed++;
+    try {
+      transformer(array[i], i, array, (result, error) => {
+        if (isErr) return;
 
-      if (completed === array.length) {
-        callback(results);
+        if (error) {
+          isErr = true;
+          callback(null, error);
+          return;
+        }
+
+        results[i] = result;
+        completed++;
+
+        if (completed === array.length) {
+          callback(results);
+        }
+      });
+    } catch (err) {
+      if (!isErr) {
+        isErr = true;
+        callback(null, err);
       }
-    });
+    }
   }
 }
 
 const squareF = (data, index, array, cb) => {
   setTimeout(() => {
-    cb(data * data);
+    if (typeof data === "number") {
+      cb(null, new Error(`incorrect datatype ${index}`));
+    } else {
+      cb(data * data);
+    }
   }, 500);
 };
 
-asyncMap(nums, squareF, (result) => {
-  console.log("Result:", result);
+asyncMap(nums, squareF, (result, error) => {
+  if (error) {
+    console.error("Error:", error.message);
+  } else {
+    console.log("Result:", result);
+  }
 });
