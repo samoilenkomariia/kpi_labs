65b9353a3da7dae7c6ff975b3a9e4286ade939ec
samoilenkomariia <mariia.samoilenko.06@gmail.com>
2024-12-29 15:23:41 +0200

    final

--- lab1.js
+++ lab1.js
@@ -1,54 +1,33 @@
 "use strict";
 
-const nums = [5, 2, 7, "sa", "sd"];
-const nums2 = [5, 4, 7, 3, 9];
+const nums = [5, 4, 7, 3, 9];
 
-// console.log(nums.map((x) => x * x)); ф-ція яку я обрала
-function asyncMap(array, transformer, callback, delay) {
+function asyncMap(array, transformer, callback) {
   let results = [];
   let completed = 0;
-  let isErr = false;
 
-  if (completed === array.length) {
-    callback(results);
-    return;
-  }
   for (let i = 0; i < array.length; i++) {
-    if (isErr) return;
-    if (typeof array[i] !== "number") {
-      return callback(new Error(`${array[i]} is NaN`), null);
-    }
-
-    transformer(array[i], i, array, (err, result) => {
-      if (err) {
-        isErr = true;
-        return callback(err);
-      }
-
+    transformer(array[i], i, array, (result) => {
       results[i] = result;
       completed++;
 
-      if (completed === array.length) callback(null, results);
+      if (completed === array.length) {
+        callback(null, results); 
+      }
     });
-    setTimeout(() => {}, delay);
   }
 }
 
 const squareF = (data, index, array, cb) => {
   setTimeout(() => {
-    cb(null, data * data);
+    cb(data * data);
   }, 500);
 };
 
-asyncMap(
-  nums,
-  squareF,
-  (err, result) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log("Result:", result);
-    }
-  },
-  500
-);
+asyncMap(nums, squareF, (err, result) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Result:", result); 
+  }
+});
